58e5b333b693eb9e9942021d033ea35f3b96aa23
Sergei Stremberg <sekispb@hotmail.com>
2024-03-01 17:17:52 +0200

    new version of motion sensor file -> for HC-SR04

--- Arduino/arduino_motion_sensor/arduino_motion_sensor.ino
+++ Arduino/arduino_motion_sensor/arduino_motion_sensor.ino
@@ -1,30 +1,46 @@
+const int DISTANCE_THRESHOLD = 300; // cm
+const int TRIGGER_PIN = 9;
+const int ECHO_PIN = 10;
 const int LED_PIN = 13;
-const int PIR_PIN = 8;
 
-bool motionDetected = false;
+bool cooldown = false;
+bool motionState = true;
 
-void setup()
-{
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(PIR_PIN, INPUT);
-  Serial.begin(9600);
+void setup() {
+    Serial.begin(9600);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(TRIGGER_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
 }
 
-void loop()
-{
-  // check current motion status
-  bool currentMotion = digitalRead(PIR_PIN) == HIGH;
+void loop() {
+    long duration;
+    long distance; // cm
 
-  if (currentMotion && !motionDetected)
-  {                                     // if motion detected and not previously detected
-    Serial.println("Motion detected!"); // print message
-    motionDetected = true;              // update motion detection flag
-    digitalWrite(LED_PIN, HIGH);        // turn LED on
-  }
-  else if (!currentMotion && motionDetected)
-  {                                  // if no motion and previously detected
-    Serial.println("Motion ended!"); // print message
-    motionDetected = false;          // update motion detection flag
-    digitalWrite(LED_PIN, LOW);      // turn LED off
-  }
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER_PIN, LOW);
+
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = duration / 59;
+
+    // NO MOTION:
+    if (distance > DISTANCE_THRESHOLD ) {
+        cooldown = false;
+        digitalWrite(LED_PIN, LOW);
+        if (motionState) {
+            Serial.println("No motion..");
+            motionState = false;
+        }
+    }
+    // MOTION DETECTED:
+    else if (distance <= DISTANCE_THRESHOLD && !cooldown) {
+        Serial.print("Motion derected at ");
+        Serial.print(distance);
+        Serial.println(" cm");
+        digitalWrite(LED_PIN, HIGH);
+        cooldown = true;
+        motionState = true;
+    }
+    delay(60);
 }
