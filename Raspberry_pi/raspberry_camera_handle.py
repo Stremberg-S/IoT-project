9c52685bf6a9c1817f4c85c156833ef97a67f241
stremberg-s <sekispb@hotmail.com>
2024-03-04 16:17:31 +0200

    changed raspberry file

--- Raspberry_pi/raspberry_camera_handle.py
+++ Raspberry_pi/raspberry_camera_handle.py
@@ -5,11 +5,11 @@ from time import sleep
 
 from serial import Serial, SerialException
 
-# from picamera import PiCamera
+from picamera import PiCamera
 
 SERIAL_PORT: str = '/dev/ttyUSB0'
 BAUD_RATE: int = 9600
-USB_DRIVE_PATH: str = '/media/pi/USB_DRIVE'
+USB_DRIVE_PATH: str = '/media/r4GUI/BB35-4AB9/raspberry'
 MOTION_DETECTED_MSG: str = 'Motion detected'
 MOTION_ENDED_MSG: str = 'No motion'
 
@@ -22,40 +22,44 @@ def setup_serial(port: str, baud_rate: int) -> Serial or None:
         return None
 
 
-def record_video(output_path: str) -> None:
-    try:
-        timestamp = dt.now().strftime('%Y-%m-%d_%H-%M-%S')
-        video_file = os.path.join(output_path, f'motion_{timestamp}.h264')
-        recording_process = Popen(['raspivid', '-o', video_file])
-        sleep(10)
-        recording_process.terminate()
-    except Exception as e:
-        print(f'Error recording video: {e}')
-
-
 # def record_video(output_path: str) -> None:
-#     """Record video using Raspberry Pi camera for 10 seconds."""
 #     try:
 #         timestamp = dt.now().strftime('%Y-%m-%d_%H-%M-%S')
 #         video_file = os.path.join(output_path, f'motion_{timestamp}.h264')
-#
-#         camera = PiCamera().start_preview()
-#         camera.start_recording(video_file)
-#         sleep(10)  # Record for 10 seconds
-#         camera.stop_recording().stop_preview().close()
+#         recording_process = Popen(['raspivid', '-o', video_file])
+#         sleep(10)
+#         recording_process.terminate()
 #     except Exception as e:
-#         print(f"Error recording video: {e}")
+#         print(f'Error recording video: {e}')
+
+
+def record_video(output_path: str) -> None:
+    """Record video using Raspberry Pi camera for 10 seconds."""
+    try:
+        timestamp = dt.now().strftime('%Y-%m-%d_%H-%M-%S')
+        video_file = os.path.join(output_path, f'motion_{timestamp}.h264')
+#
+        camera = PiCamera().start_preview()
+        camera.start_recording(video_file)
+        sleep(10)  # Record for 10 seconds
+        camera.stop_recording().stop_preview().close()
+    except Exception as e:
+        print(f"Error recording video: {e}")
 
 
 def main() -> None:
     ino_serial = setup_serial(SERIAL_PORT, BAUD_RATE)
+    print(ino_serial)
+    print('Connected')
 
     if ino_serial is None:
+        print('no connection!!')
         return
 
     try:
         while True:
             serial_data = ino_serial.readline().strip().decode('utf-8')
+            print(serial_data)
 
             if serial_data == MOTION_DETECTED_MSG:
                 print('Motion detected! Recording video...')
@@ -73,3 +77,16 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+#
+
+# import os
+# 
+# def get_usb_drive_name():
+#     devices = os.listdir('/media/r4GUI')
+#     
+#     for device in devices:
+#         print(device)
+#         
+#         
+# if __name__ == '__main__':
+#     get_usb_drive_name()
